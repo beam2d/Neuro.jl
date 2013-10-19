7a2b0987cb38f34a36c802c66e1477e67bfd3d40
Seiya Tokui <beam.web@gmail.com>
2013-10-18 19:49:18 -0700

    Fix ambiguous order of arguments of max

--- src/layer/relu_layer.jl
+++ src/layer/relu_layer.jl
@@ -3,6 +3,6 @@ export ReluLayer, fprop, grad, bprop, weight
 # Rectified linear layer.
 immutable ReluLayer{T} <: ImmutableLayer{T}; end
 
-fprop{T}(::ReluLayer{T}, in::AbstractArray{T}) = max(in, 0)
+fprop{T}(::ReluLayer{T}, in::AbstractArray{T}) = max(0, in)
 bprop{T}(::ReluLayer{T}, in::AbstractArray{T}, out::AbstractArray{T}, d_out::AbstractArray{T}) =
     (in .> 0) .* d_out
