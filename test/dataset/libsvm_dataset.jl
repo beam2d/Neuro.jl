1fa72d1224cee6b47b1b11e1e8daedd3678d49c9
Seiya Tokui <beam.web@gmail.com>
2013-10-18 22:57:26 -0700

    Make the test cases more readable

--- test/dataset/libsvm_dataset.jl
+++ test/dataset/libsvm_dataset.jl
@@ -3,46 +3,35 @@ using Neuro.Dataset
 
 let
     cases = [
-        (Float64, "libsvm_test1.txt", 0),
-        (Float64, "libsvm_test1.txt", 4),
-        (Float64, "libsvm_test2.txt", 0),
-        (Float32, "libsvm_test1.txt", 0)
+        (Float64, "libsvm_test1.txt", 0,
+         [1. 2. 3.;
+          1. 2. 3.;
+          1. 2. 3.],
+         [1, 2, 3]),
+        (Float64, "libsvm_test1.txt", 4,
+         [1. 2. 3.;
+          1. 2. 3.;
+          1. 2. 3.;
+          0. 0. 0.;],
+         [1, 2, 3]),
+        (Float64, "libsvm_test2.txt", 0,
+         [1. 0. 2.;
+          0. 3. 1.;
+          3. 1. 0.],
+         [1, 3, 2]),
+        (Float32, "libsvm_test1.txt", 0,
+         [1.f0 2.f0 3.f0;
+          1.f0 2.f0 3.f0;
+          1.f0 2.f0 3.f0],
+         [1, 2, 3])
     ]
 
-    expects = {
-        {
-            [1. 2. 3.;
-             1. 2. 3.;
-             1. 2. 3.],
-            [1, 2, 3]
-        }, {
-            [1. 2. 3.;
-             1. 2. 3.;
-             1. 2. 3.;
-             0. 0. 0.],
-            [1, 2, 3]
-        }, {
-            [1. 0. 2.;
-             0. 3. 1.;
-             3. 1. 0.],
-            [1, 3, 2]
-        }, {
-            [1.f0 2.f0 3.f0;
-             1.f0 2.f0 3.f0;
-             1.f0 2.f0 3.f0],
-            [1, 2, 3],
-        }
-    }
-
-    for (case, expect) in zip(cases, expects)
-        t, filename, dim = case
-        X_expect, y_expect = expect
-        filename = "test/dataset/test_data/" * filename
-
+    for (t, filename, dim, X_expect, y_expect) in cases
+        fn = "test/dataset/test_data/" * filename
         X, y = if dim == 0
-            read_libsvm_dataset(t, filename)
+            read_libsvm_dataset(t, fn)
         else
-            read_libsvm_dataset(t, filename, dim)
+            read_libsvm_dataset(t, fn, dim)
         end
 
         @test X_expect == X
