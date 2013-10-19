29b3ab4aa6e8eaf7cb216951885941868caeee1b
Seiya Tokui <beam.web@gmail.com>
2013-10-18 22:58:38 -0700

    Add comments on test cases

--- test/dataset/libsvm_dataset.jl
+++ test/dataset/libsvm_dataset.jl
@@ -3,22 +3,26 @@ using Neuro.Dataset
 
 let
     cases = [
+        # Usual case
         (Float64, "libsvm_test1.txt", 0,
          [1. 2. 3.;
           1. 2. 3.;
           1. 2. 3.],
          [1, 2, 3]),
+        # Explicit indication of dimension
         (Float64, "libsvm_test1.txt", 4,
          [1. 2. 3.;
           1. 2. 3.;
           1. 2. 3.;
           0. 0. 0.;],
          [1, 2, 3]),
+        # Sparse input
         (Float64, "libsvm_test2.txt", 0,
          [1. 0. 2.;
           0. 3. 1.;
           3. 1. 0.],
          [1, 3, 2]),
+        # Another element type
         (Float32, "libsvm_test1.txt", 0,
          [1.f0 2.f0 3.f0;
           1.f0 2.f0 3.f0;
